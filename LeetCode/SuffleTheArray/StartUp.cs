e372f47046b7b5f52fca8ca65745833569964ab3
Stoyko Kolev <stoikokolev@gmail.com>
2020-08-28 14:13:45 +0300

    ShuffleTheArray

--- LeetCode/SuffleTheArray/StartUp.cs
+++ LeetCode/SuffleTheArray/StartUp.cs
@@ -13,8 +13,26 @@ namespace SuffleTheArray
         {
             public int[] Shuffle(int[] nums, int n)
             {
+                var count = -1;
+                var newArr = new int[n * 2];
+                for (var i = 0; i < n * 2; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        count++;
+                        newArr[i] = nums[count];
+                    }
+                    else
+                    {
+                        newArr[i] = nums[count + n];
 
+                    }
+
+                }
+
+                return newArr;
             }
+
         }
     }
 }
