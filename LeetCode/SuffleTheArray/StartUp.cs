125a0c3e9961eb7961f3a16c4b682171bf0ec4b9
Stoyko Kolev <stoikokolev@gmail.com>
2020-08-28 14:00:23 +0300

    LeetCode

---
+++ LeetCode/SuffleTheArray/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SuffleTheArray
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"https://leetcode.com/problems/shuffle-the-array/");
+        }
+
+        public class Solution
+        {
+            public int[] Shuffle(int[] nums, int n)
+            {
+
+            }
+        }
+    }
+}
