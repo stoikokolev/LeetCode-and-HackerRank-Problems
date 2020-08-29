4b1b098dc0d17ccb3300096ebd859d8fee7afacd
Stoyko Kolev <stoikokolev@gmail.com>
2020-08-29 08:38:59 +0300

    SearchInsertPosition

---
+++ LeetCode/SearchInsertPosition/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SearchInsertPosition
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"https://leetcode.com/problems/search-insert-position/");
+        }
+
+        public int SearchInsert(int[] nums, int target)
+        {
+            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (target <= nums[i])
+                {
+                    return i;
+                }
+            }
+
+            return nums.Length;
+
+        }
+    }
+}
