5da2fc3153f94b0dcb588e30684e7c07db527548
Stoyko Kolev <stoikokolev@gmail.com>
2020-08-31 23:13:33 +0300

    Arranging Coins

---
+++ LeetCode/ArrangingCoins/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ArrangingCoins
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"https://leetcode.com/problems/arranging-coins/");
+        }
+        public int ArrangeCoins(int n)
+        {
+            var count = 0;
+            while (n >= 0)
+            {
+
+                if (count <= n)
+                {
+                    n -= count;
+                    count++;
+                }
+                else
+                { break; }
+            }
+            return count - 1;
+        }
+
+    }
+}
