43852751fcbb74c268060e875067fa3857a78b90
Stoyko Kolev <stoikokolev@gmail.com>
2020-09-02 23:17:18 +0300

    Sort Array By Parity

---
+++ LeetCode/SortArrayByParity/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortArrayByParity
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"https://leetcode.com/problems/sort-array-by-parity/");
+        }
+
+        public int[] SortArrayByParity(int[] A)
+        {
+            var evenList = new List<int>();
+            var oddList = new List<int>();
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] % 2 == 0)
+                {
+                    evenList.Add(A[i]);
+                }
+                else
+                {
+                    oddList.Add(A[i]);
+                }
+            }
+
+            for (int i = 0; i < evenList.Count; i++)
+            {
+                A[i] = evenList[i];
+            }
+
+            for (int i = evenList.Count; i < A.Length; i++)
+            {
+                A[i] = oddList[i - evenList.Count];
+            }
+
+            return A;
+        }
+    }
+}
