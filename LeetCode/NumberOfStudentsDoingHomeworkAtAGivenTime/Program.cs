24651349febe13f23d70ddecd8d32ed6e9aa7fbf
Stoyko Kolev <stoikokolev@gmail.com>
2020-09-04 15:58:54 +0300

    Number of Students Doing Homework at a Given Time

---
+++ LeetCode/NumberOfStudentsDoingHomeworkAtAGivenTime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumberOfStudentsDoingHomeworkAtAGivenTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/");
+        }
+        public int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+        {
+            byte count = 0;
+            for (byte i = 0; i < startTime.Length; i++)
+            {
+                if (queryTime <= endTime[i] && queryTime >= startTime[i])
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
