bf4ad5e974ce6a361d6bdf3bbf7dc943ee93236f
Stoyko Kolev <stoikokolev@gmail.com>
2020-09-27 13:05:36 +0300

    Number of Good Pairs

---
+++ LeetCode/NumberOfGoodPairs/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NumberOfGoodPairs
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"https://leetcode.com/problems/number-of-good-pairs/");
+        }
+
+        public int NumIdenticalPairs(int[] nums)
+        {
+            short count = 0;
+            for (var i = 0; i < nums.Length-1; i++)
+            {
+                for (var j = i+1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
