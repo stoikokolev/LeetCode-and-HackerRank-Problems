6aae8f9bebe8a3dd0292b0b46cff1b3b81140617
Stoyko Kolev <stoikokolev@gmail.com>
2020-10-04 17:36:20 +0300

    DefangingAnIPAddress 87.79% faster

---
+++ LeetCode/DefangingAnIPAddress/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace DefangingAnIPAddress
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"https://leetcode.com/problems/defanging-an-ip-address/");
+        }
+
+        public string DefangIPaddr(string address)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < address.Length; i++)
+            {
+                if (address[i] == '.')
+                {
+                    sb.Append("[.]");
+                }
+                else
+                {
+                    sb.Append(address[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
