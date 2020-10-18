0f3013c2cb708c4ffeb81986f67918842d906ad8
Stoyko Kolev <stoikokolev@gmail.com>
2020-10-18 20:30:35 +0300

    Hackerrank intermediate exam

---
+++ LeetCode/SortedSums/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SortedSums
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            var a = new List<int>(){9,5,8};
+            Console.WriteLine(SortedSum(a));
+        }
+
+        public static int SortedSum(List<int> a)
+        {
+            int sum = 0;
+            var list = new List<int>();
+            
+            
+            foreach (var num in a)
+            {
+                list.Add(num);
+                list.Sort();
+                list.Reverse();
+                sum += list.Select((t, i) => list[list.Count - 1 - i] * (i + 1)).Sum();
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ LeetCode/TasksOfPairing/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TasksOfPairing
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            var freq = new List<long>() { 5, 6, 2, 0, 3 };
+            Console.WriteLine(TaskOfPairing(freq));
+        }
+
+        private static long TaskOfPairing(List<long> freq)
+        {
+            long result = 0;
+            var remainder = false;
+            for (int i = 0; i < freq.Count; i++)
+            {
+                if (remainder)
+                {
+                    freq[i]++;
+                    remainder = false;
+                    if (freq[i] == 1)
+                    {
+                        continue;
+                    }
+                }
+                if (freq[i] == 0)
+                {
+                    continue;
+                }
+
+                if (freq[i] % 2 != 0)
+                {
+                    if (freq[i] / 2 > 0)
+                    {
+                        remainder = true;
+                    }
+                    
+                }
+
+                result += freq[i] / 2;
+
+            }
+
+            return result;
+        }
+    }
+}
